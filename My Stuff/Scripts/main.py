e4bb97166c06cda91a135343ab7abb158d0db6c4
t3chy <elam.day.friedland@gmail.com>
2020-04-11 21:50:50 -0700

    kinda got the ticker price thing to work too

--- My Stuff/Scripts/main.py
+++ My Stuff/Scripts/main.py
@@ -1,9 +1,10 @@
 import requests
 import tweepy
-import datetime
+from datetime import datetime, timedelta, date
 import csv
 import pandas as pd
-import pandas_datareader.data as web
+import re
+from pandas_datareader import data
 consumer_key = "wBlchO1ZEiGnv1ly1wmVplNr5"
 consumer_secret = "iyHcmJITDblWRf4Zj46r19UVbngnU8DoXkBoXCtchZpZ7kF6HT"
 access_key = "1214224530341629952-RpXwF26MvZ0KEBJTcYzwEhKHgE4vQx"
@@ -13,24 +14,31 @@ auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
 dir = r"C:\Users\Techy\Anaconda3\envs\poltweet\My Stuff\Data"
 def get_tweets(username):
+    global tmp
     tmp = []
     for tweet in api.user_timeline(screen_name = username,count=200):
         if not tweet.retweeted:
                 if not "RT @" in tweet.text:
                     tmp.append(tweet)
     outtweets = [[(tweet.created_at).strftime('%m/%d/%Y'), tweet.text.encode("utf-8")] for tweet in tmp]
-    dir = r"C:\Users\Techy\Anaconda3\envs\poltweet\My Stuff\Data" + "/" + username + "_tweets" + ".txt" 
+    dir = r"C:\Users\Techy\Anaconda3\envs\poltweet\My Stuff\Data" + "/" + username + "_tweets" + ".csv" 
     with open(dir, 'w') as f:
 	    writer = csv.writer(f)
 	    writer.writerow(["created_at","text"])
 	    writer.writerows(outtweets)
 def get_stonks(ticker):
-    pass
+ #   LastDateRegex = re.compile(r'/(0[1-9]|1[012])[- \/.](0[1-9]|[12][0-9]|3[01])[- \/.](19|20)\d\d/') 
+ #   mo = LastDateRegex.search()
+     print(data.DataReader(ticker, 
+                       start=((tmp[-1].created_at)- timedelta(days=1)).strftime('%m/%d/%Y'), 
+                       end=((date.today())+timedelta(days=1)), 
+                       data_source='yahoo')['Close'])
 class company():
     def __init__(self,ticker,twitter):
            self.ticker = ticker
            self.twitter = twitter
     def main(self):
         get_tweets(self.twitter)
+        get_stonks(self.ticker)
 c = company('voo','realdonaldtrump')        
 c.main()
