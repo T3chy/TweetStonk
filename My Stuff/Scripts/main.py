510c041acb0abb2556899da38438e1a5a6358e3c
t3chy <elam.day.friedland@gmail.com>
2020-04-12 10:35:33 -0700

    did the stock stuff TODO sentiment analysis day by day and graphing it

--- My Stuff/Scripts/main.py
+++ My Stuff/Scripts/main.py
@@ -13,32 +13,61 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
 dir = r"C:\Users\Techy\Anaconda3\envs\poltweet\My Stuff\Data"
-def get_tweets(username):
+tmp = []
+def get_tweets(username, initamount):
+    amount = initamount
     global tmp
     tmp = []
-    for tweet in api.user_timeline(screen_name = username,count=200):
-        if not tweet.retweeted:
-                if not "RT @" in tweet.text:
-                    tmp.append(tweet)
+    def gather(count):
+        for tweet in api.user_timeline(screen_name = username,count=count):
+            if not tweet.retweeted:
+                    if not "RT @" in tweet.text:
+                        if (tweet.created_at).weekday() <= 5:
+                            tmp.append(tweet)  
+    if initamount <= 200:
+        gather(amount)
+    else:
+        while amount > 0:
+            if amount > 200:
+                gather(200)
+                amount = amount - 200
+            else:
+                gather(amount)  
     outtweets = [[(tweet.created_at).strftime('%m/%d/%Y'), tweet.text.encode("utf-8")] for tweet in tmp]
     dir = r"C:\Users\Techy\Anaconda3\envs\poltweet\My Stuff\Data" + "/" + username + "_tweets" + ".csv" 
     with open(dir, 'w') as f:
 	    writer = csv.writer(f)
 	    writer.writerow(["created_at","text"])
 	    writer.writerows(outtweets)
-def get_stonks(ticker):
- #   LastDateRegex = re.compile(r'/(0[1-9]|1[012])[- \/.](0[1-9]|[12][0-9]|3[01])[- \/.](19|20)\d\d/') 
- #   mo = LastDateRegex.search()
-     print(data.DataReader(ticker, 
+def get_stonks(ticker,username):
+    datas = []
+    df = data.DataReader(ticker, 
                        start=((tmp[-1].created_at)- timedelta(days=1)).strftime('%m/%d/%Y'), 
                        end=((date.today())+timedelta(days=1)), 
-                       data_source='yahoo')['Close'])
+                       data_source='yahoo')
+    df.reset_index(inplace=True,drop=False)
+    for index, row in df.iterrows():
+        datas.append([row['Date'].strftime('%m/%d/%Y'),row['Close']])
+    dir = r"C:\Users\Techy\Anaconda3\envs\poltweet\My Stuff\Data" + "/" + username + "_prices" + ".csv" 
+    print(datas)   
+    with open(dir, 'w') as f:
+	    writer = csv.writer(f)
+	    writer.writerow(["Date","Price"])
+	    writer.writerows(datas)
 class company():
-    def __init__(self,ticker,twitter):
+    def __init__(self,ticker,twitter,numtwt):
            self.ticker = ticker
            self.twitter = twitter
+           self.numtwt = numtwt
     def main(self):
-        get_tweets(self.twitter)
-        get_stonks(self.ticker)
-c = company('voo','realdonaldtrump')        
-c.main()
+        get_tweets(self.twitter,self.numtwt)
+        get_stonks(self.ticker,self.twitter)
+companies = input("how many companies would you like to analyze?")
+for i in range(int(companies)):
+    companyt = input("Corperate twitter handle?")
+    ticker = input("stock ticker?")
+    twtnum = input("how many tweets would you like to use for analysis (default:200)")
+    if twtnum == "":
+        twtnum = 200
+    c = company(ticker,companyt,int(twtnum))        
+    c.main()
