d504a4b04598f8ab4b419a36c47f5b021c90c3a0
t3chy <elam.day.friedland@gmail.com>
2020-04-16 09:22:34 -0700

    finally got the data homoginized by date just have to sort out the indexing stuff

--- My Stuff/Scripts/graph.py
+++ My Stuff/Scripts/graph.py
@@ -3,22 +3,23 @@ import pandas as pd
 def graphem(username):
     #### Gather Data and Import as Data Frames
     dir = r"C:\Users\Techy\Anaconda3\envs\poltweet\My Stuff\Data" + "/" + username + "_polarity" + ".csv" 
-    stk = pd.read_csv(dir)
-    dir = r"C:\Users\Techy\Anaconda3\envs\poltweet\My Stuff\Data" + "/" + username + "_prices" + ".csv"
     pol = pd.read_csv(dir)
+    dir = r"C:\Users\Techy\Anaconda3\envs\poltweet\My Stuff\Data" + "/" + username + "_prices" + ".csv"
+    stk = pd.read_csv(dir)
     stk = stk.reindex(index=stk.index[::-1])
     #### Concatenate data making sure only data points contained on both lists are proccessed
-    for i in pol.iterrows():
+    pos = 0
+    for i in pol.Date.isin(stk.Date).astype(int):
         print(i)
-        if i[0] == "Date":
-            continue
-        if not (i[0] in stk):
-            pol.drop(i['Date'])
-    for i in stk.iterrows():
-        if i[0] == "Date":
-            continue
-        if not (i[0] in pol):
-            stk.drop(i)
+        #print(pos)
+        if i == 0:
+            pol = pol.drop([pos])
+        pos = pos + 1
+    pos = len(stk.index) -1
+    for i in stk.Date.isin(pol.Date).astype(int):
+        if i == 0:
+            stk = stk.drop([pos])
+        pos = pos - 1
     print(pol)
     print(stk)
 graphem('Microsoft')
